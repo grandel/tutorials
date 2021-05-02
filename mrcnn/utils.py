512cd42454fed0976af4ace04b8ae6389f8d9f78
tomasz.rak <tomasz.rak@wundermanthompson.com>
2021-05-02 22:46:59 +0200

    added baloon config and baloon dataset

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -236,6 +236,9 @@ def box_refinement(box, gt_box):
 #  Dataset
 ############################################################
 
+
+
+
 class Dataset(object):
     """The base class for dataset classes.
     To use it, create a new class that adds functions specific to the dataset
--- samples/shapes/train_shapes.py
+++ samples/shapes/train_shapes.py
@@ -6,9 +6,8 @@
 # 
 # This notebook shows how to train Mask R-CNN on your own dataset. To keep things simple we use a synthetic dataset of shapes (squares, triangles, and circles) which enables fast training. You'd still need a GPU, though, because the network backbone is a Resnet101, which would be too slow to train on a CPU. On a GPU, you can start to get okay-ish results in a few minutes, and good results in less than an hour.
 # 
-# The code of the *Shapes* dataset is included below. It generates images on the fly, so it doesn't require downloading any data. And it can generate images of any size, so we pick a small image size to train faster. 
-
-
+# The code of the *Shapes* dataset is included below. It generates images on the fly, so it doesn't require downloading any data. And it can generate images of any size, so we pick a small image size to train faster.
+import json
 import os
 import sys
 import random
@@ -17,6 +16,7 @@ import numpy as np
 import cv2
 
 # Root directory of the project
+import skimage
 from matplotlib import pyplot as plt
 
 ROOT_DIR = os.path.abspath("../../")
@@ -31,6 +31,27 @@ from mrcnn.model import log
 
 
 # get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+class BalloonConfig(Config):
+    """Configuration for training on the toy  dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "balloon"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + balloon
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
 
 
 class ShapesConfig(Config):
@@ -100,6 +121,107 @@ def display_random_images(dataset_train):
         image = dataset_train.load_image(image_id)
         mask, class_ids = dataset_train.load_mask(image_id)
         # visualize.display_top_masks(image, mask, class_ids, dataset_train.class_names)
+
+
+class BalloonDataset(utils.Dataset):
+
+    def load_balloon(self, dataset_dir, subset):
+        """Load a subset of the Balloon dataset.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("balloon", 1, "balloon")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # Load annotations
+        # VGG Image Annotator (up to version 1.6) saves each image in the form:
+        # { 'filename': '28503151_5b5b7ec140_b.jpg',
+        #   'regions': {
+        #       '0': {
+        #           'region_attributes': {},
+        #           'shape_attributes': {
+        #               'all_points_x': [...],
+        #               'all_points_y': [...],
+        #               'name': 'polygon'}},
+        #       ... more regions ...
+        #   },
+        #   'size': 100202
+        # }
+        # We mostly care about the x and y coordinates of each region
+        # Note: In VIA 2.0, regions was changed from a dict to a list.
+        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+        annotations = list(annotations.values())  # don't need the dict keys
+
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # The if condition is needed to support VIA versions 1.x and 2.x.
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']]
+
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "balloon",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a balloon dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "balloon":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            try:
+                rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+                mask[rr, cc, i] = 1
+            except Exception as e:
+                print("index out of bound in mask array", e)
+
+
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask, np.ones([mask.shape[-1]], dtype=np.int32)
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "balloon":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
 
 
 class ShapesDataset(utils.Dataset):
@@ -230,10 +352,10 @@ class ShapesDataset(utils.Dataset):
         return bg_color, shapes
 
 
-def create_model():
+def create_model(model_dir, config, coco_model_path):
     # Create model in training mode
     model = modellib.MaskRCNN(mode="training", config=config,
-                              model_dir=MODEL_DIR)
+                              model_dir=model_dir)
 
     # Which weights to start with?
     init_with = "coco"  # imagenet, coco, or last
@@ -244,7 +366,7 @@ def create_model():
         # Load weights trained on MS COCO, but skip layers that
         # are different due to the different number of classes
         # See README for instructions to download the COCO weights
-        model.load_weights(COCO_MODEL_PATH, by_name=True,
+        model.load_weights(coco_model_path, by_name=True,
                            exclude=["mrcnn_class_logits", "mrcnn_bbox_fc",
                                     "mrcnn_bbox", "mrcnn_mask"])
     elif init_with == "last":
@@ -261,7 +383,7 @@ def create_model():
 # 2. Fine-tune all layers. For this simple example it's not necessary, but we're including it to show the process. Simply pass `layers="all` to train all layers.
 
 
-def train_heads(model):
+def train_heads(model, dataset_train, dataset_val, config):
     # Train the head branches
     # Passing layers="heads" freezes all layers except the head
     # layers. You can also pass a regular expression to select
@@ -270,7 +392,7 @@ def train_heads(model):
     return model
 
 
-def train_all(model):
+def train_all(model, dataset_train, dataset_val, config):
     # Fine tune all layers
     # Passing layers="all" trains all layers. You can also
     # pass a regular expression to select which layers to
@@ -293,11 +415,11 @@ class InferenceConfig(ShapesConfig):
     IMAGES_PER_GPU = 1
 
 
-def infere(config):
+def infere(config, model_dir, dataset_train, dataset_val):
     # Recreate the model in inference mode
     model = modellib.MaskRCNN(mode="inference",
                               config=config,
-                              model_dir=MODEL_DIR)
+                              model_dir=model_dir)
 
     # Get path to saved weights
     # Either set a specific path or find last trained weights
@@ -319,17 +441,19 @@ def infere(config):
     log("gt_bbox", gt_bbox)
     log("gt_mask", gt_mask)
 
-    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, dataset_train.class_names, figsize=(8, 8))
+    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, dataset_train.class_names,
+                                figsize=(8, 8))
 
     results = model.detect([original_image], verbose=1)
 
     r = results[0]
-    visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], dataset_val.class_names, r['scores'], ax=get_ax())
+    visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], dataset_val.class_names,
+                                r['scores'], ax=get_ax())
 
 
 # ## Evaluation
 
-def evalute(model, config):
+def evalute(model, config, dataset_val):
     # Compute VOC-Style mAP @ IoU=0.5
     # Running on 10 images. Increase for better accuracy.
     image_ids = np.random.choice(dataset_val.image_ids, 10)
@@ -350,34 +474,49 @@ def evalute(model, config):
     print("mAP: ", np.mean(APs))
 
 
-if __name__ == '__main__':
+def main():
     # Directory to save logs and trained model
-    MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+    model_dir = os.path.join(ROOT_DIR, "logs")
 
     # Local path to trained weights file
-    COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+    coco_model_path = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
     # Download COCO trained weights from Releases if needed
-    if not os.path.exists(COCO_MODEL_PATH):
-        utils.download_trained_weights(COCO_MODEL_PATH)
+    if not os.path.exists(coco_model_path):
+        utils.download_trained_weights(coco_model_path)
 
-    config = ShapesConfig()
+    # config = ShapesConfig()
+    config = BalloonConfig()
     config.display()
 
     # Training dataset
-    dataset_train = ShapesDataset()
-    dataset_train.load_shapes(500, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    # dataset_train = ShapesDataset()
+    # dataset_train.load_shapes(500, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    # dataset_train.prepare()
+    #
+    # # Validation dataset
+    # dataset_val = ShapesDataset()
+    # dataset_val.load_shapes(50, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    # dataset_val.prepare()
+
+    baloon_dataset_dir = "./../../datasets/balloon"
+    dataset_train = BalloonDataset()
+    dataset_train.load_balloon(baloon_dataset_dir, "train")
     dataset_train.prepare()
 
     # Validation dataset
-    dataset_val = ShapesDataset()
-    dataset_val.load_shapes(50, config.IMAGE_SHAPE[0], config.IMAGE_SHAPE[1])
+    dataset_val = BalloonDataset()
+    dataset_val.load_balloon(baloon_dataset_dir, "val")
     dataset_val.prepare()
 
     display_random_images(dataset_train)
 
-    model = create_model()
-    model = train_heads(model)
-    # model = train_all(model)
-    inference_config = InferenceConfig()
-    infere(inference_config)
-    # evalute(model)
+    model = create_model(model_dir, config, coco_model_path)
+    model = train_heads(model, dataset_train, dataset_val, config)
+    # model = train_all(model, dataset_train, dataset_val, config)
+    # inference_config = InferenceConfig()
+    # infere(inference_config, model_dir, dataset_train, dataset_val)
+    evalute(model, config, dataset_val)
+
+
+if __name__ == '__main__':
+    main()
